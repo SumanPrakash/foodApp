8ca8a9954a9447ec71ace52f7511b6c596b21ba5
Suman Prakash <suman@Sumans-MacBook-Pro.local>
2020-11-26 10:12:40 +0530

    Added recipe models and other things

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Pasta',
+      'Cheesy pineapple pasta',
+      'https://www.jessicagavin.com/wp-content/uploads/2018/03/cobb-salad-7B-1200.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
