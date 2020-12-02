f281214d627b1d2078acfb367261e167a7c56805
Prakash <y509447@softwareag.com>
2020-12-02 15:34:19 +0530

    Splitted the app into different parts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './recipes/recipe.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  featureLoaded: string = 'recipe';
+  recipeLoaded: Recipe;
+  onNavigate(feature: string) {
+    this.featureLoaded = feature;
+  }
+  onRecipeSelect(recipe) {
+    this.recipeLoaded = recipe;
+  }
+}
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Recipe } from './../recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -13,9 +13,25 @@ export class RecipeListComponent implements OnInit {
       'Cheesy pineapple pasta',
       'https://www.jessicagavin.com/wp-content/uploads/2018/03/cobb-salad-7B-1200.jpg'
     ),
+    new Recipe(
+      'Recipe1',
+      'This is recipe 1',
+      'https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/04/shakshuka-egg-1296x728-header.jpg?w=1155&h=1528'
+    ),
+    new Recipe(
+      'Recipe2',
+      'This is recipe 2',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBjoC0llcLubuz4fC0oCTfuA03V_fsqqPgSA&usqp=CAU'
+    ),
   ];
+
+  @Output() recipeSelected = new EventEmitter<Recipe>();
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
+
+  onSelect(recipe) {
+    this.recipeSelected.emit(recipe);
+  }
 }
